fab38ad4bc2fcabbacf7dbd1de6fed7952380907
arstanrakhmat <arstanbek.rakh@gmail.com>
2024-10-02 00:14:54 +0600

    Create a function to handle states

---
+++ androidApp/src/main/java/com/example/noteappkmm/android/note_list/NoteListViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.noteappkmm.android.note_list
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.noteappkmm.data.note.SearchNotes
+import com.example.noteappkmm.domain.note.Note
+import com.example.noteappkmm.domain.note.NoteDataSource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class NoteListViewModel @Inject constructor(
+    private val noteDataSource: NoteDataSource,
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val searchNotes = SearchNotes()
+
+    private val notes = savedStateHandle.getStateFlow("notes", emptyList<Note>())
+    private val searchText = savedStateHandle.getStateFlow("searchText", "")
+    private val isSearchActive = savedStateHandle.getStateFlow("isSearchActive", false)
+
+    val state = combine(notes, searchText, isSearchActive) { notes, searchText, isSearchActive ->
+        NoteListState(
+            notes = searchNotes.execute(notes, searchText),
+            searchText = searchText,
+            isSearchActive = isSearchActive
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), NoteListState())
+}
